4385b6890029aa0bb787fffd536765360cf245e0
Arun Balasubramanian <aarunbala@gmail.com>
2017-06-07 18:39:17 +1000

    Updating the comment in the DEVELOP branch

--- HelloGit/server.js
+++ HelloGit/server.js
@@ -1,9 +1,7 @@
 var http = require("http");
 
-
-
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("It's alive!");
+  response.write("It's alive but in a diff branch!");
   response.end();
 }).listen(3000);
