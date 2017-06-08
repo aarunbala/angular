0a383e2c46d301b58d5ddac5e62891fea6827738
Arun Balasubramanian <aarunbala@gmail.com>
2017-06-08 19:04:31 +1000

    Angular project using JHipster

---
+++ JHipsterAngular/src/main/java/com/angular/bingo/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.angular.bingo.repository;
+
+import com.angular.bingo.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ JHipsterAngular/src/main/java/com/angular/bingo/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,19 @@
+package com.angular.bingo.security;
+
+import com.angular.bingo.config.Constants;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of AuditorAware based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public String getCurrentAuditor() {
+        String userName = SecurityUtils.getCurrentUserLogin();
+        return userName != null ? userName : Constants.SYSTEM_ACCOUNT;
+    }
+}
